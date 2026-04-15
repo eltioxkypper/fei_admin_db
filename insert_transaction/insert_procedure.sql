cbcc4c25610fef5ef9d735758e0544391a88ce98
Max Millán <139258361+eltioxkypper@users.noreply.github.com>
2026-04-15 09:08:49 -0600

    Add stored procedure for region insertion
    Create a stored procedure for inserting a region with error handling.

--- insert_transaction/insert_procedure.sql
+++ insert_transaction/insert_procedure.sql
@@ -1,1 +1,21 @@
+CREATE PROCEDURE [stored_procedure_name]
+	@var1 INT,
+	@status INT OUTPUT,
+	@message NVARCHAR(MAX) OUTPUT, 
+	@id INT OUTPUT
+AS
+BEGIN TRY
+	BEGIN TRANSACTION 
+		INSERT INTO Region VALUES (@region_id, @region_description);
+	COMMIT TRANSACTION
+
+	SET @status = 200;
+	SET @message = 'Ok';
+	SET @id = @@IDENTITY;
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION;
+	SET @status = 500;
+	SET @message = ERROR_MESSAGE();
+END CATCH
 
