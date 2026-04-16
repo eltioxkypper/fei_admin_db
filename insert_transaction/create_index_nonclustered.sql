142ab10ee36bd79be96cbc076e10215eefa7d84f
Max Millán <139258361+eltioxkypper@users.noreply.github.com>
2026-04-16 07:42:42 -0600

    Enhance SQL script with additional index creation examples
    Added multiple nonclustered index creation examples, including support for duplicate values and ordering.

--- insert_transaction/create_index_nonclustered.sql
+++ insert_transaction/create_index_nonclustered.sql
@@ -1,12 +1,30 @@
 -- creación de un índice con múltiples columnas para valores únicos
-CREATE UNIQUE NONCLUSTERED [name_index]
+CREATE UNIQUE NONCLUSTERED INDEX [name_index]
 ON [name_table] ([name_column_1],[name_column_2],[name_column_3]);
 GO
 
 -- creación de un índice con una sola columna para valores únicos
-CREATE UNIQUE NONCLUSTERED [name_index]
+CREATE UNIQUE NONCLUSTERED INDEX [name_index]
 ON [name_table] ([name_column]);
 GO
+
+-- creación de un índice que acepta valores duplicados
+CREATE NONCLUSTERED INDEX [name_index]
+ON [name_table] ([name_column]);
+GO
+
+-- creación de índices ordenados ASC o DESC
+CREATE NONCLUSTERED INDEX [name_index]
+ON [name_table] (
+  [name_column] ASC
+);
+GO 
+
+CREATE NONCLUSTERED INDEX [name_index]
+ON [name_table] (
+  [name_column] DESC
+);
+GO 
 
 -- delete unique nonclustered index
 DROP INDEX [name_index] ON [schema_name].[name_table]
